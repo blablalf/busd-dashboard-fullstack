3197a9163bdc951bdd8cc31903bc72a65f5b1f11
blablalf <a.gaillard@ra2.tech>
2024-10-07 13:47:59 +0200

    chore: changing frontend port

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://127.0.0.1:80',
+    origin: 'http://127.0.0.1:8080',
   });
   app.setGlobalPrefix('api');
   await app.listen(3000);
