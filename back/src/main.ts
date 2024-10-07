3e6de0ebec82f0e72e108201ac5e546a6cac3ca8
blablalf <a.gaillard@ra2.tech>
2024-10-07 16:51:33 +0200

    fix: removing the CORS on the back

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://127.0.0.1:8080',
-  });
+  // Same network so not needed
+  // app.enableCors({
+  //   origin: 'http://127.0.0.1:8080',
+  // });
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
