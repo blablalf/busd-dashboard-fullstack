d4e0471e5e743993b68fc016acdf3252944e5536
blablalf <a.gaillard@ra2.tech>
2024-10-07 08:51:05 +0200

    fix: Add db creation and fix cors port into backend

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://127.0.0.1:5173',
+    origin: 'http://127.0.0.1:80',
   });
   app.setGlobalPrefix('api');
   await app.listen(3000);
